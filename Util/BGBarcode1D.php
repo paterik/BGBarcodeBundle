0bb762b4aeb95c3ae4408a832917b583143f4b58
Patrick Paechnatz <patrick.paechnatz@googlemail.com>
2014-06-29 11:48:56 +0200

    feature, autoload refactoring and naming issues

---
+++ Util/BGBarcode1D.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of bitgrave's barcode wrapper bundle based on our barcode generator BGBarcodeGenerator,
+ * https://github.com/paterik/BGBarcodeGenerator
+ *
+ * branch: https://github.com/paterik/BGBarcodeBundle
+ * stable version: 1.0.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BG\Barcode;
+
+/**
+ * class BGBarcode1D 1.0.0
+ * 1D barcode class
+ *
+ * @author Patrick Paechnatz, https://github.com/paterik
+ */
+class BGBarcode1D extends Base1DBarcode
+{
+}
---
+++ Util/BGBarcode2D.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of bitgrave's barcode wrapper bundle based on our barcode generator BGBarcodeGenerator,
+ * https://github.com/paterik/BGBarcodeGenerator
+ *
+ * branch: https://github.com/paterik/BGBarcodeBundle
+ * stable version: 1.0.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BG\Barcode;
+
+/**
+ * class BGBarcode2D 1.0.0
+ * 2D matrix barcode base class
+ *
+ * @author Patrick Paechnatz, https://github.com/paterik
+ */
+class BGBarcode2D extends Base2DBarcode
+{
+}
