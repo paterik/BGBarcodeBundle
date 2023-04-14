ed390928e4a9ca1cee1fcb8013aaf55da153224a
Tomas Votruba <tomas.vot@gmail.com>
2023-04-14 13:13:29 +0200

    clean resources

---
+++ ecs.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        SetList::SPACES,
+        SetList::ARRAY,
+        SetList::DOCBLOCK,
+        SetList::NAMESPACES,
+        SetList::COMMENTS,
+        SetList::PSR_12,
+    ]);
+};
